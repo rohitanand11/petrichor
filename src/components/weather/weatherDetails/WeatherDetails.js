39d72372808bc2eba0580c5549c47204504816b1
Rohit Anand <rohitanand3456@gmail.com>
2020-06-29 00:43:40 +0530

    fixed errors in weatherDetailsComponent.

--- src/components/weather/weatherDetails/WeatherDetails.js
+++ src/components/weather/weatherDetails/WeatherDetails.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import classes from './WeatherDetails.module.css';
+import React from "react";
+import classes from "./WeatherDetails.module.css";
 
-const WeatherDetails = () => {
-    return(
-        <div className={classes.WeatherDetailsContainer}>
-            {/* all the details */}
-        </div>
+const WeatherDetails = (props) => {
+
+  const createListItems = () => {
+    return (
+      <ul className={classes.ulStyle}>
+        <li>City : {props.pData.location.name}</li>
+        <li>Temprature : {props.pData.current.temperature} degree cel</li>
+        <li>Humidity : {props.pData.current.humidity}</li>
+        <li>Day Time : {props.pData.current.is_day}</li>
+        <li>Pressure : {props.pData.current.pressure}</li>
+        <li>Local Time:{props.pData.location.localtime}</li>
+        <li>Visiblity : {props.pData.current.visibility}</li>
+      </ul>
     );
-}
+  };
+
+  return (
+  <div className={classes.WeatherDetailsContainer}>
+      {createListItems()}
+  </div>);
+};
 
 export default WeatherDetails;
