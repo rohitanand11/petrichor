933109466a096cd5696a6c659f8df1450df269c3
Rohit Anand <rohitanand3456@gmail.com>
2020-06-28 21:07:06 +0530

    created new component WeatherDetails

---
+++ src/components/weather/weatherDetails/WeatherDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './WeatherDetails.module.css';
+
+const WeatherDetails = () => {
+    return(
+        <div className={classes.WeatherDetailsContainer}>
+            {/* all the details */}
+        </div>
+    );
+}
+
+export default WeatherDetails;
