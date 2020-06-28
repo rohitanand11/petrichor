6656b278a190d877331c25821fac71763a4ce85a
Rohit Anand <rohitanand3456@gmail.com>
2020-06-28 17:45:22 +0530

    added logic for search bar in app js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,62 @@
 import React, { Component } from "react";
-import Header from './components/Header/Header';
+import Header from "./components/Header/Header";
 import Clouds from "./components/clouds/Clouds";
-import SearchBar from './components/search_bar/searchBar';
+import SearchBar from "./components/search_bar/searchBar";
 
-import './assets/icons/Icons';
+import "./assets/icons/Icons";
 import city_array from "./assets/data/citiesArray";
 import classes from "./App.module.css";
 
-
 class App extends Component {
-
   state = {
-    textValue:'',
-    suggestions:[...city_array],
-  }
+    textValue: "",
+    suggestions: [],
+    // searchArrowClick: false,
+  };
 
   handleTextValue = (event) => {
     const value_input = event.target.value;
 
-    if(value_input.length()===0){
-      this.setState({textValue:value_input,suggestions:[]});
+    if (value_input.length === 0) {
+      this.setState({ textValue: value_input, suggestions: [] });
     } else {
-    const regex = new RegExp(`${value_input}`,'i');
-    const suggest_list = this.state.suggestions.filter((el)=>el.test(regex));
-    this.setState({textValue:value_input,suggestions:suggest_list});
+      const regex = new RegExp(`${value_input}`, "i");
+      const suggest_list = city_array.filter((el) => regex.test(el));
+      const firstFourSuggestion = this.firstFourSuggestion(suggest_list);
+      this.setState({
+        textValue: value_input,
+        suggestions: firstFourSuggestion,
+      });
     }
-  }
+  };
+
+  firstFourSuggestion = (pFilteredSuggestions) => {
+    const fourSuggestions = [];
+    for (let i = 0; i < 4; i++) {
+      fourSuggestions.push(pFilteredSuggestions[i]);
+    }
+    return fourSuggestions;
+  };
+
+  // handlesearchArrowClick = () => {
+  //   this.setState({
+  //     textValue: "",
+  //     suggestions: [],
+  //     searchArrowClick: true,
+  //   });
+  // };
 
   render() {
     return (
       <React.Fragment>
         <div className={classes.container}></div>
-        <Header/>
-        <Clouds/>
-        <SearchBar textValue={this.state.textValue} change={this.handleTextValue} suggestions={this.state.suggestions}/>
+        <Header />
+        <Clouds />
+        <SearchBar
+          textValue={this.state.textValue}
+          change={this.handleTextValue}
+          suggestions={this.state.suggestions}
+        />
       </React.Fragment>
     );
   }
