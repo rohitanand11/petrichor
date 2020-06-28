c6b51e3c99807c8821d26f7afb67bb32a239ae50
Rohit Anand <rohitanand3456@gmail.com>
2020-06-28 10:56:58 +0530

    added a new component Clouds

--- src/components/clouds/Clouds.js
+++ src/components/clouds/Clouds.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import classes from "./Clouds.module.css"
+import classes from "./Clouds.module.css";
 
 const Clouds = () => {
-    return(
-        <div>
-            <FontAwesomeIcon icon="cloud" size="10x" className={classes.cloud1}/>
-        </div>
-
-        
-    );
-}
+  return (
+    <div className={classes.cloud}>
+      <div className={classes.cloud1}>
+        <FontAwesomeIcon icon="cloud"  color="black" size="7x"/>
+      </div>
+      <div className={classes.cloud2}>
+        <FontAwesomeIcon icon="cloud"  color="white" size="10x"/>
+      </div>
+      <div className={classes.cloud3}>
+        <FontAwesomeIcon icon="cloud"  color="gray" size="2x"/>
+      </div>
+    </div>
+  );
+};
 
 export default Clouds;
