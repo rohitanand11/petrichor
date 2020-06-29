80c6e65ac2f40951d1672ad588d31cc36c32af00
Rohit Anand <rohitanand3456@gmail.com>
2020-06-29 20:43:14 +0530

    Added build folder contents inside docs folder for deployment.
    	~added homepage in package json file.

---
+++ docs/precache-manifest.9c5fa814ef3fd3a8f716822ecb8c87b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1270855085fded03d8452bc40e64a9c",
+    "url": "/Petrichor/index.html"
+  },
+  {
+    "revision": "527f558e8af6dbb338cd",
+    "url": "/Petrichor/static/css/main.dec346ef.chunk.css"
+  },
+  {
+    "revision": "e8693ad1e666afda5f16",
+    "url": "/Petrichor/static/js/2.f5d9dce2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Petrichor/static/js/2.f5d9dce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "527f558e8af6dbb338cd",
+    "url": "/Petrichor/static/js/main.fdb48b1d.chunk.js"
+  },
+  {
+    "revision": "0d4b498fcd9717fcd0a1",
+    "url": "/Petrichor/static/js/runtime-main.2ba8bf13.js"
+  }
+]);
