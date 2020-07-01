d1d20d85478f5ce47d22ea90c765eae1682507fb
Rohit Anand <rohitanand3456@gmail.com>
2020-07-02 00:19:01 +0530

    Modified weather component to take in weather heading.

--- src/components/weather/weatherContainer/Weather.js
+++ src/components/weather/weatherContainer/Weather.js
@@ -2,10 +2,12 @@ import React from 'react';
 import classes from './Weather.module.css';
 import WeatherImage from '../weatherImage/WeatherImage';
 import WeatherDetails from '../weatherDetails/WeatherDetails';
+import WeatherHeading from '../weather_heading/WeatherHeading';
 
 const Weather = (props) => {
     return(
-        <div>
+        <div className={classes.WeatherMainContainer}>
+            <WeatherHeading/>
             <div className={classes.weatherContainer}>
                 <WeatherImage pData={props.pData}/>
                 <WeatherDetails pData={props.pData}/>
