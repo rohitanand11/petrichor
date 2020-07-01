48180c6c5c176cebc6da5d7ab6cbdb7257e7d302
Rohit Anand <rohitanand3456@gmail.com>
2020-07-02 00:17:24 +0530

    Modified the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,11 +6,8 @@ const Header = () => {
     <div className={classes.container}>
       <h3 className={classes.brandName}>Petrichor</h3>
       <ul className={classes.links}>
-        <li>link1</li>
-        <li>link2</li>
-        <li>link3</li>
-        <li>link4</li>
-        <li>link5</li>
+        <li>Mitti Di Khushboo</li>
+        
       </ul>
     </div>
   );
